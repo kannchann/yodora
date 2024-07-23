2cbe45174df883a3f4b6c88563223051c30fb2ef
kannchann <kannchann.thapa@gmail.com>
2024-07-23 22:30:00 +0545

    navbar

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-//to toggle the navbar menu on mobile
 document.addEventListener("DOMContentLoaded", function () {
   const navbarToggle = document.getElementById("navbar-toggle");
   const navbarMenu = document.getElementById("navbar-menu");
 
   navbarToggle.addEventListener("click", function () {
+    navbarToggle.classList.toggle("active");
     navbarMenu.classList.toggle("active");
   });
 });
